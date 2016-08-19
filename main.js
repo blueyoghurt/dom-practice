e392d9bdf9d8f9601d73f7a7bc2a60282b0ac334
Luke <hello@lukeonthebrightside.com>
2016-08-19 09:58:58 +0800

    first submission

--- main.js
+++ main.js
@@ -1,38 +1,74 @@
-console.log("hello world!");
-
-function setInputAsTitle() {
-  var title = document.getElementById("title");
-  var input = document.getElementsByTagName("input")[0];
-
-  var value = input.value;
-  var result = 2 * value;
-
-  title.textContent = result;
-}
+// function setInputAsTitle() {
+//   var title = document.getElementById("title");
+//   var input = document.getElementsByTagName("input")[0];
+//
+//   var value = input.value;
+//   var result = 2 * value;
+//
+//   title.textContent = result;
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log("DOM loaded");
 
-  // attach a click handler to the set title button
-  document.getElementById("set-title").addEventListener("click", function() {
-    console.log("set title button clicked");
-    setInputAsTitle();
+  // attach a click handler to the set submitConvert button
+  document.getElementById("submitConvert").addEventListener("click", function() {
+    console.log('clicked');
+    // Check which radio button is selected and apply formula accordingy
+    var optionSelected = document.getElementsByClassName("radioButton");
+      if (optionSelected[0].checked){
+        convertToF();
+        } else {
+          convertToC();
+      }
   });
 
-  // attach a click handler to the blue button
-  document.getElementById("blue").addEventListener("click", function() {
-    console.log("blue button clicked");
-    var block = document.getElementById("block");
-    block.classList.remove("red");
-    block.classList.add("blue");
-  });
+  // attach a click handler to the clear button
+  // document.getElementById("clear").addEventListener("click", function() {
+  //   console.log("blue button clicked");
+  //   var block = document.getElementById("block");
+  //   block.classList.remove("red");
+  //   block.classList.add("blue");
+  // });
+
+
 
   // attach a click handler to the red button
-  document.getElementById("red").addEventListener("click", function() {
-    console.log("red button clicked");
-    var block = document.getElementById("block");
-    block.classList.remove("blue");
-    block.classList.add("red");
-  });
+  // document.getElementById("red").addEventListener("click", function() {
+  //   console.log("red button clicked");
+  //   var block = document.getElementById("block");
+  //   block.classList.remove("blue");
+  //   block.classList.add("red");
+  // });
 
+  function convertToC () {
+    var input = document.getElementById("value-to-convert");
+    var convertC = document.getElementById("results");
+
+    var result = (input.value - 32) / 1.8;
+    convertC.textContent = result;
+    backgroundColor(result,input.value);
+    console.log(result, " C");
+  }
+
+  function convertToF () {
+    var input = document.getElementById("value-to-convert");
+    var convertC = document.getElementById("results");
+
+    var result = (input.value * 1.8) +32;
+    convertC.textContent = result;
+    backgroundColor(input.value,result);
+    console.log(result, " F");
+  }
+
+  function backgroundColor(x,y) {
+    if (x > 30 || y > 86) {
+      document.getElementById("results-box").style.backgroundColor = "Red";
+      console.log("turnRed");
+    } else {
+      document.getElementById("results-box").style.backgroundColor = "Blue";
+      console.log("turnBlue");
+      }
+
+  }
 });
